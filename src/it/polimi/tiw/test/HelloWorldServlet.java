9a603027cc3534de76fa5b409e03fdefb6b5a536
Tancredi Covioli <mr.coviols96@gmail.com>
2020-03-15 18:22:41 +0100

    base

---
+++ src/it/polimi/tiw/test/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package it.polimi.tiw.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
